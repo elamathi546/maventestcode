d2577377ceef04227bba8736ab7ec36ff79525d1
Elamathi <elamathikalasi744@gmail.com>
2024-05-12 13:19:41 +0530

    First commit

---
+++ src/main/java/com/mavan/proj2/sampleproj2/App.java
@@ -0,0 +1,13 @@
+package com.mavan.proj2.sampleproj2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
